37ea000ed47e1cb12673aff1a0ce85ec47799998
jseee <61853660+jseee@users.noreply.github.com>
2021-04-08 20:18:05 +0200

    Create Asignatura.java

--- src/main/java/tarea1/jpa/Asignatura.java
+++ src/main/java/tarea1/jpa/Asignatura.java
@@ -35,131 +35,113 @@ public class Asignatura implements Serializable {
 	public Asignatura(){
 		
 	}
-
-	public Long getReferencia() {
-		return referencia;
+	
+	public Long getReferencia()
+	{
+		return this.referencia;
 	}
-
-	public void setReferencia(Long referencia) {
+	
+	public void setReferencia(Long referencia)
+	{
 		this.referencia = referencia;
 	}
-
-	public Long getCodigo() {
-		return codigo;
+	
+	public Long getCodigo()
+	{
+		return this.codigo;
 	}
-
-	public void setCodigo(Long codigo) {
-		this.codigo = codigo;
+	
+	public void setCodigo(Long codigo)
+	{
+		this.referencia = referencia;
 	}
-
+	
 	public Integer getCreditos_totales() {
-		return creditos_totales;
+		return this.creditos_totales;
 	}
 
-	public void setCreditos_totales(Integer creditos_totales) {
-		this.creditos_totales = creditos_totales;
-	}
-
+	public void setCreditos_totales(Integer Creditos_totales) {
+		this.creditos_totales = Creditos_totales;
+	}   
 	public Integer getCreditos_practicos() {
-		return creditos_practicos;
+		return this.creditos_practicos;
 	}
 
-	public void setCreditos_practicos(Integer creditos_practicos) {
-		this.creditos_practicos = creditos_practicos;
-	}
-
+	public void setCreditos_practicos(Integer Creditos_practicos) {
+		this.creditos_practicos = Creditos_practicos;
+	}   
 	public Integer getCreditos_teoricos() {
-		return creditos_teoricos;
+		return this.creditos_teoricos;
 	}
 
-	public void setCreditos_teoricos(Integer creditos_teoricos) {
-		this.creditos_teoricos = creditos_teoricos;
+	public void setCreditos_teoricos(Integer Creditos_teoricos) {
+		this.creditos_teoricos = Creditos_teoricos;
 	}
-
+	
 	public Integer getOferta() {
-		return oferta;
+		return this.oferta;
 	}
 
-	public void setOferta(Integer oferta) {
-		this.oferta = oferta;
-	}
-
+	public void setOferta(Integer Oferta) {
+		this.oferta = Oferta;
+	}   
 	public String getNombre() {
-		return nombre;
+		return this.nombre;
 	}
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
+	public void setNombre(String Nombre) {
+		this.nombre = Nombre;
+	}   
 	public String getCurso() {
-		return curso;
+		return this.Curso;
 	}
 
-	public void setCurso(String curso) {
-		this.curso = curso;
-	}
-
+	public void setCurso(String Curso) {
+		this.curso = Curso;
+	}   
 	public String getCaracter() {
-		return caracter;
+		return this.caracter;
 	}
 
-	public void setCaracter(String caracter) {
-		this.caracter = caracter;
-	}
-
+	public void setCaracter(String Caracter) {
+		this.caracter = Caracter;
+	}   
 	public String getDuracion() {
-		return duracion;
+		return this.duracion;
 	}
 
-	public void setDuracion(String duracion) {
-		this.duracion = duracion;
-	}
-
+	public void setDuracion(String Duracion) {
+		this.duracion = Duracion;
+	}   
 	public String getUnidad_temporal() {
-		return unidad_temporal;
+		return this.unidad_temporal;
 	}
 
 	public void setUnidad_temporal(String unidad_temporal) {
 		this.unidad_temporal = unidad_temporal;
-	}
-
+	}   
 	public String getIdiomas() {
-		return idiomas;
+		return this.Idiomas;
 	}
 
 	public void setIdiomas(String idiomas) {
 		this.idiomas = idiomas;
 	}
-
-	public Grupo_asignatura getGrupo_asignatura() {
-		return grupo_asignatura;
-	}
-
-	public void setGrupo_asignatura(Grupo_asignatura grupo_asignatura) {
-		this.grupo_asignatura = grupo_asignatura;
-	}
-
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((caracter == null) ? 0 : caracter.hashCode());
-		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
-		result = prime * result + ((creditos_practicos == null) ? 0 : creditos_practicos.hashCode());
-		result = prime * result + ((creditos_teoricos == null) ? 0 : creditos_teoricos.hashCode());
-		result = prime * result + ((creditos_totales == null) ? 0 : creditos_totales.hashCode());
-		result = prime * result + ((curso == null) ? 0 : curso.hashCode());
-		result = prime * result + ((duracion == null) ? 0 : duracion.hashCode());
-		result = prime * result + ((grupo_asignatura == null) ? 0 : grupo_asignatura.hashCode());
-		result = prime * result + ((idiomas == null) ? 0 : idiomas.hashCode());
+		result = prime * result + codigo;
+		result = prime * result + creditos_practicos;
+		result = prime * result + creditos_teoricos;
+		result = prime * result + creditos_totales;
 		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
-		result = prime * result + ((oferta == null) ? 0 : oferta.hashCode());
-		result = prime * result + ((referencia == null) ? 0 : referencia.hashCode());
-		result = prime * result + ((unidad_temporal == null) ? 0 : unidad_temporal.hashCode());
+		result = prime * result + oferta;
+		result = prime * result + referencia;
 		return result;
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -169,82 +151,44 @@ public class Asignatura implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Asignatura other = (Asignatura) obj;
-		if (caracter == null) {
-			if (other.caracter != null)
-				return false;
-		} else if (!caracter.equals(other.caracter))
+		if (codigo != other.codigo)
 			return false;
-		if (codigo == null) {
-			if (other.codigo != null)
-				return false;
-		} else if (!codigo.equals(other.codigo))
+		if (creditos_practicos != other.creditos_practicos)
 			return false;
-		if (creditos_practicos == null) {
-			if (other.creditos_practicos != null)
-				return false;
-		} else if (!creditos_practicos.equals(other.creditos_practicos))
+		if (creditos_teoricos != other.creditos_teoricos)
 			return false;
-		if (creditos_teoricos == null) {
-			if (other.creditos_teoricos != null)
-				return false;
-		} else if (!creditos_teoricos.equals(other.creditos_teoricos))
+		if (creditos_totales != other.creditos_totales)
 			return false;
-		if (creditos_totales == null) {
-			if (other.creditos_totales != null)
-				return false;
-		} else if (!creditos_totales.equals(other.creditos_totales))
-			return false;
-		if (curso == null) {
-			if (other.curso != null)
-				return false;
-		} else if (!curso.equals(other.curso))
-			return false;
-		if (duracion == null) {
-			if (other.duracion != null)
-				return false;
-		} else if (!duracion.equals(other.duracion))
-			return false;
-		if (grupo_asignatura == null) {
-			if (other.grupo_asignatura != null)
-				return false;
-		} else if (!grupo_asignatura.equals(other.grupo_asignatura))
-			return false;
-		if (idiomas == null) {
-			if (other.idiomas != null)
-				return false;
-		} else if (!idiomas.equals(other.idiomas))
-			return false;
-		if (nombre == null) {
-			if (other.nombre != null)
+		if (Nombre == null) {
+			if (other.Nombre != null)
 				return false;
 		} else if (!nombre.equals(other.nombre))
 			return false;
-		if (oferta == null) {
-			if (other.oferta != null)
-				return false;
-		} else if (!oferta.equals(other.oferta))
+		if (oferta != other.oferta)
 			return false;
-		if (referencia == null) {
-			if (other.referencia != null)
-				return false;
-		} else if (!referencia.equals(other.referencia))
-			return false;
-		if (unidad_temporal == null) {
-			if (other.unidad_temporal != null)
-				return false;
-		} else if (!unidad_temporal.equals(other.unidad_temporal))
+		if (referencia != other.referencia)
 			return false;
 		return true;
 	}
-
+	
 	@Override
 	public String toString() {
-		return "Asignatura [referencia=" + referencia + ", codigo=" + codigo + ", creditos_totales=" + creditos_totales
-				+ ", creditos_practicos=" + creditos_practicos + ", creditos_teoricos=" + creditos_teoricos
-				+ ", oferta=" + oferta + ", nombre=" + nombre + ", curso=" + curso + ", caracter=" + caracter
-				+ ", duracion=" + duracion + ", unidad_temporal=" + unidad_temporal + ", idiomas=" + idiomas
-				+ ", grupo_asignatura=" + grupo_asignatura + "]";
+		StringBuilder builder = new StringBuilder();
+		builder.append("Asignatura [Referencia=");
+		builder.append(referencia);
+		builder.append(", Codigo=");
+		builder.append(codigo);
+		builder.append(", Creditos_totales=");
+		builder.append(creditos_totales);
+		builder.append(", Creditos_practicos=");
+		builder.append(creditos_practicos);
+		builder.append(", Creditos_teoricos=");
+		builder.append(creditos_teoricos);
+		builder.append(", Oferta=");
+		builder.append(oferta);
+		builder.append(", Nombre=");
+		builder.append(nombre);
+		builder.append("]");
+		return builder.toString();
 	}
-	
-	
 }
