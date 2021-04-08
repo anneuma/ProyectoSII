bc891bc22f3831393c8fe5ce11b44c340e066ef4
feelnostalgic <franciscoaragones@uma.es>
2021-04-08 12:22:00 -0700

    Versión DDL

--- src/main/java/tarea1/jpa/Optativa_.java
+++ src/main/java/tarea1/jpa/Optativa_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2021-04-08T12:08:33.926-0700")
+@Generated(value="Dali", date="2021-04-08T12:20:20.690-0700")
 @StaticMetamodel(Optativa.class)
 public class Optativa_ extends Asignatura_ {
 	public static volatile SingularAttribute<Optativa, Integer> Plazas;
