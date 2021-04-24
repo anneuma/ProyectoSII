fabf3cab83c4649da43a5d646ebb51fcd6fc5df8
Daniel Campillos Llamas <62467448+DanielCLL@users.noreply.github.com>
2021-04-24 13:12:21 +0200

    Create ExpedienteEJB.java

---
+++ src/main/java/tarea2/ejb/ExpedienteEJB.java
@@ -0,0 +1,41 @@
+package tarea2.ejb;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import tarea1.jpa.Expediente;
+import tarea2.exception.*;
+
+@Stateless
+public class AsignaturaEJB implements GestionAsignatura {
+	@PersistenceContext(name="ProyectoSII")
+	private EntityManager em;
+	
+	@Override
+	public void modificarExpediente (Expediente expediente) throws ExpedienteNoEncontradoException {
+		Expediente expedienteEntity = em.find(Expediente.class, expediente.getId());
+		if (expedienteEntity == null) {
+			throw new ExpedienteNoEncontradoException();
+		}
+		em.merge(expediente);
+	}
+	
+	@Override
+	public void eliminarExpediente (Expediente expediente) throws ExpedienteNoEncontradoException {
+		Expediente expedienteEntity = em.find(Expediente.class, expediente.getId());
+		if (expedienteEntity == null) {
+			throw new ExpedienteNoEncontradoException();
+		}
+		em.remove(em.merge(expediente));
+	}
+	
+	@Override
+	public List<Asignatura> consultarAsignatura(Asignatura asignatura) throws ExpedienteNoEncontradoException {
+		Expediente expedienteEntity = em.find(Expediente.class, expediente.getId());
+		if (expedienteEntity == null) {
+			throw new ExpedienteNoEncontradoException();
+		}
+		return expediente.getExpedientes();
+	}
+}
