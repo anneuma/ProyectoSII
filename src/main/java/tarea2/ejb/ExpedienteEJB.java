da2711484b88a438c60b469abcf0287309acfdfd
Daniel Campillos Llamas <62467448+DanielCLL@users.noreply.github.com>
2021-04-24 13:50:14 +0200

    Update ExpedienteEJB.java

--- src/main/java/tarea2/ejb/ExpedienteEJB.java
+++ src/main/java/tarea2/ejb/ExpedienteEJB.java
@@ -5,7 +5,7 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import tarea1.jpa.Expediente;
-import tarea2.exception.ExpedienteNoEncontradoException;
+import tarea2.exception.*;
 
 @Stateless
 public class AsignaturaEJB implements GestionExpediente {
@@ -28,5 +28,41 @@ public class AsignaturaEJB implements GestionExpediente {
 			throw new ExpedienteNoEncontradoException();
 		}
 		em.remove(em.merge(expediente));
+	}
+	
+	@Override
+	public List<Titulacion> obtenerTitulacion(Titulacion titulacion) throws TitulacionNoEncontradaException {
+		Titulacion titulacionEntity = em.find(Titulacion.class, titulacion.getId());
+		if (titulacionEntity == null) {
+			throw new TitulacionNoEncontradaException();
+		}
+		return titulacion.getTitulaciones();
+	}
+	
+	@Override
+	public Encuesta obtenerEncuesta (Encuesta encuesta) throws EncuestaNoEncontradaException {
+		Encuesta encuestaEntity = em.find(Encuesta.class, encuesta.getId());
+		if (encuestaEntity == null) {
+			throw new EncuestaNoEncontradaException();
+		}
+		return encuesta.getEncuestas();
+	}
+	
+	@Override
+	public List<Alumno> obtenerAlumno (Alumno alumno) throws AlumnoNoEncontradoException {
+		Alumno alumnoEntity = em.find(Alumno.class, alumno.getId());
+		if (alumnoEntity == null) {
+			throw new AlumnoNoEncontradoException();
+		}
+		return alumno.getAlumnos();
+	}
+	
+	@Override
+	public List<Matricula> obtenerMatricula (Matricula matricula) throws MatriculaNoEncontradaException {
+		Matricula matriculaEntity = em.find(Matricula.class, matricula.getId());
+		if (matriculaEntity == null) {
+			throw new MatriculaNoEncontradaException();
+		}
+		return matricula.getMatriculas();
 	}
 }
