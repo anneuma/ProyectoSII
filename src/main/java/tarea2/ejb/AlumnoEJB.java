379ed6b328ae2bc1be71ae904c33ba1834e90135
jseee <61853660+jseee@users.noreply.github.com>
2021-04-24 02:56:35 -0700

    Add files via upload

---
+++ src/main/java/tarea2/ejb/AlumnoEJB.java
@@ -0,0 +1,69 @@
+package tarea2.ejb;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import tarea1.jpa.Alumno;
+import tarea1.jpa.Expediente;
+import tarea2.exception.*;
+
+// @autor: Jose Vargas
+
+@Stateless
+public class AlumnoEJB implements GestionAlumno {
+	
+	@PersistenceContext (name="ProyectoSII")
+	private EntityManager em;
+	
+	@Override
+	public void insertarAlumno(Alumno alumno) throws AlumnoExisteException
+	{
+		Alumno alumnoentity = em.find(Alumno.class, alumno.getId());
+		if (alumnoentity != null)
+		{
+			throw new AlumnoExisteException("El alumno ya existe")
+		}
+		
+		em.persist(alumno);
+	}
+	
+	@Override
+	public void actualizarAlumno(Alumno alumno) throws AlumnoNoExisteException
+	{
+		Alumno alumnoentity = em.find(Alumno.class, alumno.getId());
+		if (alumnoentity == null)
+		{
+			throw new AlumnoNoExisteException("El alumno no existe");
+		}
+		
+		em.merge(alumno);
+	}
+	
+	@Override
+	public void eliminarAlumno(Alumno alumno) throws AlumnoNoExistenteException
+	{
+		Alumno alumnoentity = em.find(Alumno.class, alumno.getId());
+		if (alumnoentity == null)
+		{
+			throw new AlumnoNoExisteException("El alumno no existe");
+		}
+		
+		em.remove(em.merge(alumno));
+	}
+	
+	@Override
+	public void List<Expediente> obtenerExpedientes(Alumno alumno) AlumnoNoExistenteException
+	{
+		Alumno alumnoentity = em.find(Alumno.class, alumno.getId());
+		if (alumnoentity == null)
+		{
+			throw new AlumnoNoExisteException("El alumno no existe");
+		}
+		
+		return alumno.getExpedientes();
+	}
+	
+	
+	
+}
