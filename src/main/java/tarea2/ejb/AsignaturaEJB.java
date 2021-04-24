278d2a5c224bade2be52a99bf265d4d2c0c2ad5f
Daniel Campillos Llamas <62467448+DanielCLL@users.noreply.github.com>
2021-04-24 13:49:18 +0200

    Update AsignaturaEJB.java

--- src/main/java/tarea2/ejb/AsignaturaEJB.java
+++ src/main/java/tarea2/ejb/AsignaturaEJB.java
@@ -4,12 +4,12 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import tarea1.jpa.Asignatura;
-import tarea2.exception.AsignaturaNoEncontradaException;
+import tarea1.jpa.*;
+import tarea2.exception.*;
 
 @Stateless
 public class AsignaturaEJB implements GestionAsignatura {
-  @PersistenceContext(name="ProyectoSII")
+	@PersistenceContext(name="ProyectoSII")
 	private EntityManager em;
 	
 	@Override
@@ -28,5 +28,32 @@ public class AsignaturaEJB implements GestionAsignatura {
 			throw new AsignaturaNoEncontradaException();
 		}
 		em.remove(em.merge(asignatura));
+	}
+	
+	@Override
+	public List<Titulacion> obtenerTitulacion(Titulacion titulacion) throws TitulacionNoEncontradaException {
+		Titulacion titulacionEntity = em.find(Titulacion.class, titulacion.getId());
+		if (titulacionEntity == null) {
+			throw new TitulacionNoEncontradaException();
+		}
+		return titulacion.getTitulaciones();
+	}
+	
+	@Override
+	public GrupoAsignatura obtenerGrupoAsignatura(Grupo grupo) throws GrupoNoEncontradoException {
+		Grupo grupoEntity = em.find(Grupo.class, grupo.getId());
+		if (grupoEntity == null) {
+			throw new GrupoNoEncontradoException();
+		}
+		return grupo.getGrupos();
+	}
+	
+	@Override
+	public List<Clase> obtenerClase (Clase clase) throws ClaseNoEncontradaException {
+		Clase claseEntity = em.find(Clase.class, clase.getId());
+		if (claseEntity == null) {
+			throw new ClaseNoEncontradaException();
+		}
+		return clase.getClases();
 	}
 }
