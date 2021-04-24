e2509b66aaeca873603caf1e68bec1fca6c6f8fc
Daniel Campillos Llamas <62467448+DanielCLL@users.noreply.github.com>
2021-04-24 13:10:03 +0200

    Create AsignaturaEJB.java

---
+++ src/main/java/tarea2/ejb/AsignaturaEJB.java
@@ -0,0 +1,41 @@
+package tarea2.ejb;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import tarea1.jpa.Asignatura;
+import tarea2.exception.*;
+
+@Stateless
+public class AsignaturaEJB implements GestionAsignatura {
+  @PersistenceContext(name="ProyectoSII")
+	private EntityManager em;
+	
+	@Override
+	public void modificarAsignatura (Asignatura asignatura) throws AsignaturaNoEncontradaException {
+		Asignatura asignaturaEntity = em.find(Asignatura.class, asignatura.getId());
+		if (asignaturaEntity == null) {
+			throw new AsignaturaNoEncontradaException();
+		}
+		em.merge(asignatura);
+	}
+	
+	@Override
+	public void eliminarAsignatura (Asignatura asignatura) throws AsignaturaNoEncontradaException {
+		Asignatura asignaturaEntity = em.find(Asignatura.class, asignatura.getId());
+		if (asignaturaEntity == null) {
+			throw new AsignaturaNoEncontradaException();
+		}
+		em.remove(em.merge(asignatura));
+	}
+	
+	@Override
+	public List<Asignatura> consultarAsignatura(Asignatura asignatura) throws AsignaturaNoEncontradaException {
+		Asignatura asignaturaEntity = em.find(Asignatura.class, asignatura.getId());
+		if (asignaturaEntity == null) {
+			throw new AsignaturaNoEncontradaException();
+		}
+		return asignatura.getAsignaturas();
+	}
+}
